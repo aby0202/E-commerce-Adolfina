dfe52a9329e895ab29c07caecde871d3b0af8780
Agustina Yañez <yaneza538@gmail.com>
2022-08-01 18:06:29 -0300

    fetch ,no anda carrito

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
+
 let titulo = document.getElementById("header");
 titulo.innerHTML = "<h1>Adolfina Decoracion</h1>";
 
 let texto = document.getElementById("texto");
 texto.innerHTML = "<h2>¿Quienes somos?</h2><p>Adolfina Decoracion surgio como un proyecto de hermanas. Desde chicas nos encantaba coser, diseñar, mezclar texturas y colores</p>";
 
-class Producto {
-    constructor(id, nombre, precio, cantidad, tamaño) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.cantidad = cantidad;
-        this.tamaño = tamaño;
-        this.img = img;
-    }
+
+const contenedorProductos = document.getElementById("contenedor-productos")
+
+function obtenerProducto(){
+    fetch("https://62e746fe0e5d74566af1490e.mockapi.io/productos")
+    .then((response) => {
+        console.log(response);
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        console.log(data[0].body);
+
+    data.forEach((producto) => {
+    const div = document.createElement('div')
+    div.className = "col-md-4 mt-3";
+    div.id = `div-${producto.id}`;
+    div.classList.add('producto')
+    div.innerHTML = `
+    <div class="card">
+        <div class="card-body">
+        <h3 class="card-title">${producto.id}.${producto.nombre}</h5>
+        <p class="card-text">Precio: <b>$ ${producto.precio}</b></p>
+        <p class="card-text">Tamaño: <b>${producto.descripcion}<b></p>
+        <button id= "agregar${producto.id}" class="boton-agregar">Agregar <i class = "fas fa-shopping-cart"></i></button>
+        </div>
+    </div>`
+
+    contenedorProductos.appendChild(div);
+
+    const boton = document.getElementById(`agregar${producto.id}`)
+
+    boton.addEventListener('click', () => {
+        Toastify({
+        text: `Agregaste ${producto.nombre} al carrito de compras`,
+        className: "info",
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+    }).showToast();
+        agregarCarrito(producto.id)
+    })
+})
+    });
+
+    let data = [];
+    
 }
 
-const contenedorProductos = document.getElementById('contenedor-productos')
+
+obtenerProducto()
+//me muestra la info del producto por esto
+
+
 
 const contenedorCarrito = document.getElementById('carrito-contenedor')
 
@@ -63,68 +106,6 @@ const cantidadTotal = document.getElementById('cantidadTotal')
 
 let carrito = []
 
-let catalogoProductos = [];
-
-catalogoProductos.push(new Producto(1, "Almohadon de pana ", 700, 1, "40x40 cm", img = 'imagenes/almohadonpana.jpg'));
-catalogoProductos.push(new Producto(2, "Almohadon de tusor ", 700, 1, "40x40 cm", img = 'imagenes/almtusor.jpg'));
-catalogoProductos.push(new Producto(3, "Almohadon de gasa ", 800, 1, "50x30 cm", img = 'imagenes/gasaalm.jpg'));
-catalogoProductos.push(new Producto(4, "Almohada para cama ", 1300, 1, "50x40 cm", img = 'imagenes/almcama.jpg'));
-catalogoProductos.push(new Producto(5, "Cortinas de gasa ", 13000, 1, "200x150 cm", img = 'imagenes/cortinas.jpg'));
-catalogoProductos.push(new Producto(6, "Cortinas de tusor ", 15000, 1, "200x150 cm", img = 'imagenes/cortinas2.jpg'));
-catalogoProductos.push(new Producto(7, "Cortinas de lino ", 17000, 1, "200x150 cm"));
-catalogoProductos.push(new Producto(8, "Cortinas de Blackout ", 18000, 1, "200x150 cm"));
-catalogoProductos.push(new Producto(9, "Cortinas de baño ", 5000, 1, "180x150 cm", img = 'imagenes/coortbaño.jpg'));
-catalogoProductos.push(new Producto(10, "Mantel de tusor ", 5000, 1, "200x150 cm", img = 'imagenes/manteltuss.jpg'));
-catalogoProductos.push(new Producto(11, "Mantel waffle ", 7000, 1, "200x150 cm"));
-catalogoProductos.push(new Producto(12, "Mantel gasa ", 5000, 1, "200x150 cm", img = 'imagenes/gasam.jpg'));
-catalogoProductos.push(new Producto(13, "Delantal ", 1500, 1, "70x40 cm", img = 'imagenes/delantal.jpg'));
-catalogoProductos.push(new Producto(14, "Juego de servilletas x 8 ", 4000, 1, "40x40 cm", img = 'imagenes/servilleta.jpg'));
-catalogoProductos.push(new Producto(15, "Juego sabanas para cama king ", 20000, 1, "200 x 200 cm", img = 'imagenes/cama.jpg'));
-catalogoProductos.push(new Producto(16, "Juego sabanas para cama Queen ", 18000, 1, "160 x 200 cm", img = 'imagenes/cama2.jpg'));
-catalogoProductos.push(new Producto(17, "Juego sabanas para cama 2 plazas y media ", 14000, 1, "140 x 190cm", img = 'imagenes/cama.jpg'));
-catalogoProductos.push(new Producto(18, "Juego sabanas para cama 1 plaza y media ", 10000, 1, "190 x 090 cm", img = 'imagenes/cama1p.jpg'));
-catalogoProductos.push(new Producto(19, "Pie de cama ", 9000, 1, "200 x 150 cm", img = 'imagenes/piecama.jpg'));
-
-//////
-
-
-catalogoProductos.forEach((producto) => {
-    const div = document.createElement('div')
-    div.className = "col-md-4 mt-3";
-    div.id = `div-${producto.id}`;
-    div.classList.add('producto')
-    div.innerHTML = `
-    <div class="card">
-        <div class="card-body">
-        <img src=${producto.img} alt= precio>
-        <h3 class="card-title">${producto.id}.${producto.nombre}</h5>
-        <p class="card-text">Precio: <b>$ ${producto.precio}</b></p>
-        <p class="card-text">Tamaño: <b>${producto.tamaño}<b></p>
-        <button id= "agregar${producto.id}" class="boton-agregar">Agregar <i class = "fas fa-shopping-cart"></i></button>
-        </div>
-    </div>`
-
-    //  <img src= ${producto.img} alt=precio>
-    contenedorProductos.appendChild(div);
-
-    const boton = document.getElementById(`agregar${producto.id}`)
-
-    boton.addEventListener('click', () => {
-        Toastify({
-        text: `Agregaste ${producto.nombre} al carrito de compras`,
-        className: "info",
-        style: {
-            background: "linear-gradient(to right, #00b09b, #96c93d)",
-        },
-    }).showToast();
-        agregarCarrito(producto.id)
-    })
-})
-
-
-////
-
-
 const agregarCarrito = (prodId) => {
     const existe = carrito.some(prod => prod.id === prodId)
     if (existe) {
@@ -133,12 +114,11 @@ const agregarCarrito = (prodId) => {
 
         })
     } else {
-        const item = catalogoProductos.find((prod) => prod.id === prodId)
+        const item = obtenerProducto.find((prod) => prod.id === prodId)
         carrito.push(item)
     }
     actualizarCarrito()
 }
-
 
 const eliminarDelCarrito = (prodId) => {
     const item = carrito.find((prod) => prod.id === prodId)
@@ -149,16 +129,16 @@ const eliminarDelCarrito = (prodId) => {
 }
 
 const actualizarCarrito = () => {
-    contenedorCarrito.innerHTML = precio
+    contenedorCarrito.innerHTML = ""
 
     carrito.forEach((prod) => {
         const div = document.createElement('div')
         div.className = ('productoEnCarrito')
         div.innerHTML = `
-    <p>${prod.nombre}</p>
-    <p>Precio : ${prod.precio}</p>
-    <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
-    <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`
+    <p>${producto.nombre}</p>
+    <p>Precio : ${producto.precio}</p>
+    <p>Cantidad: <span id="cantidad">${producto.cantidad}</span></p>
+    <button onclick="eliminarDelCarrito(${producto.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`
 
         contenedorCarrito.appendChild(div)
 
@@ -170,3 +150,4 @@ const actualizarCarrito = () => {
     precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
     //rest parameters
 }
+

f14c5ad3401702e880303e1029a9b94d81e77271
Agustina Yañez <yaneza538@gmail.com>
2022-08-15 20:21:18 -0300

    ultimos arreglos

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -83,6 +83,32 @@ function jsonProducto() {
                 actualizarCarrito();
             }
 
+            
+            pagar.addEventListener("click", () => {
+                Swal.fire({
+                    title: "Estas seguro que quieres concluir tu compra?",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "yes",
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        Swal.fire(
+                            "listo!",
+                            "Tu compra fue realizada con exito,<br> gracias por confiar en Adolfina Decoracion",
+                            "success"
+                        );
+                        eliminar();
+                    }
+                });
+            });
+
+            function eliminar() {
+                carrito.length = 0;
+                actualizarCarrito();
+            }
+
             let carrito = [];
 
             const agregarCarrito = (prodId) => {
@@ -98,13 +124,7 @@ function jsonProducto() {
                 actualizarCarrito();
             };
 
-            const eliminarDelCarrito = (prodId) => {
-                const item = carrito.find((prod) => prod.id === prodId);
-                const indice = carrito.indexOf(item);
-                carrito.splice(indice, 1);
-                actualizarCarrito();
-                console.log(carrito);
-            };
+
 
             const actualizarCarrito = () => {
                 contenedorCarrito.innerHTML = "";
@@ -115,8 +135,7 @@ function jsonProducto() {
                     div.innerHTML = `
                         <p>${prod.nombre}</p>
                         <p>Precio : ${prod.precio}</p>
-                        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
-                        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`;
+                        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>`;
 
                     contenedorCarrito.appendChild(div);
 
@@ -137,122 +156,10 @@ function jsonProducto() {
 
 jsonProducto();
 
-// function obtenerCarrito(){
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     localStorage.getItem('carrito') && (carrito = JSON.parse(localStorage.getItem('carrito')))
-//     actualizarCarrito();
-// })
-// // operador logico AND
-
-// botonVaciar.addEventListener('click', () => {
-//     Swal.fire({
-//         title: 'Estas seguro que queres borrar el carrito de compras?',
-//         icon: 'warning',
-//         showCancelButton: true,
-//         confirmButtonColor: '#3085d6',
-//         cancelButtonColor: '#d33',
-//         confirmButtonText: 'Yes, delete it!'
-//     }).then((result) => {
-//         if (result.isConfirmed) {
-//             Swal.fire(
-//                 'Eliminado!',
-//                 'El carrito de compras fue eliminado con exito.',
-//                 'success'
-//             )
-//             eliminar()
-//         }
-//     })
-// })
-
-// function eliminar() {
-//     carrito.length = 0
-//     actualizarCarrito()
-// }
-
-// let carrito = []
-
-// const agregarCarrito = (prodId) => {
-//     const existe = carrito.some(prod => prod.id === prodId)
-//     if (existe) {
-//         const prod = carrito.map(prod => {
-//             prod.id === prodId && prod.cantidad++
-
-//         })
-//     } else {
-//         const item = obtenerProducto(catalogoProductos).find((prod) => prod.id === prodId)
-//         carrito.push(item)
-//     }
-//     actualizarCarrito()
-// }
-
-// const eliminarDelCarrito = (prodId) => {
-//     const item = carrito.find((prod) => prod.id === prodId)
-//     const indice = carrito.indexOf(item)
-//     carrito.splice(indice, 1)
-//     actualizarCarrito()
-//     console.log(carrito)
-// }
-
-// const actualizarCarrito = () => {
-//     contenedorCarrito.innerHTML = ""
-
-//     carrito.forEach((prod) => {
-//         const div = document.createElement('div')
-//         div.className = ('productoEnCarrito')
-//         div.innerHTML = `
-//     <p>${producto.nombre}</p>
-//     <p>Precio : ${producto.precio}</p>
-//     <p>Cantidad: <span id="cantidad">${producto.cantidad}</span></p>
-//     <button onclick="eliminarDelCarrito(${producto.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`
-
-//         contenedorCarrito.appendChild(div)
-
-//         localStorage.setItem('carrito', JSON.stringify(carrito))
-//     })
-
-//     contadorCarrito.innerText = carrito.length
-//     console.log(carrito)
-//     precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
-//     //rest parameters
-// }
-
-// }
-
 const contenedorCarrito = document.getElementById("carrito-contenedor");
 
 const botonVaciar = document.getElementById("vaciar-carrito");
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     localStorage.getItem('carrito') && (carrito = JSON.parse(localStorage.getItem('carrito')))
-//     actualizarCarrito();
-// })
-//operador logico AND
-
-// botonVaciar.addEventListener('click', () => {
-//     Swal.fire({
-//         title: 'Estas seguro que queres borrar el carrito de compras?',
-//         icon: 'warning',
-//         showCancelButton: true,
-//         confirmButtonColor: '#3085d6',
-//         cancelButtonColor: '#d33',
-//         confirmButtonText: 'Yes, delete it!'
-//     }).then((result) => {
-//         if (result.isConfirmed) {
-//             Swal.fire(
-//                 'Eliminado!',
-//                 'El carrito de compras fue eliminado con exito.',
-//                 'success'
-//             )
-//             eliminar()
-//         }
-//     })
-// })
-
-// function eliminar() {
-//     carrito.length = 0
-//     actualizarCarrito()
-// }
 
 const contadorCarrito = document.getElementById("contadorCarrito");
 
@@ -260,49 +167,4 @@ const cantidad = document.getElementById("cantidad");
 const precioTotal = document.getElementById("precioTotal");
 const cantidadTotal = document.getElementById("cantidadTotal");
 
-// let carrito = []
-
-// const agregarCarrito = (prodId) => {
-//     const existe = carrito.some(prod => prod.id === prodId)
-//     if (existe) {
-//         const prod = carrito.map(prod => {
-//             prod.id === prodId && prod.cantidad++
-
-//         })
-//     } else {
-//         const item = obtenerProducto.find((prod) => prod.id === prodId)
-//         carrito.push(item)
-//     }
-//     actualizarCarrito()
-// }
-
-// const eliminarDelCarrito = (prodId) => {
-//     const item = carrito.find((prod) => prod.id === prodId)
-//     const indice = carrito.indexOf(item)
-//     carrito.splice(indice, 1)
-//     actualizarCarrito()
-//     console.log(carrito)
-// }
-
-// const actualizarCarrito = () => {
-//     contenedorCarrito.innerHTML = ""
-
-//     carrito.forEach((prod) => {
-//         const div = document.createElement('div')
-//         div.className = ('productoEnCarrito')
-//         div.innerHTML = `
-//     <p>${producto.nombre}</p>
-//     <p>Precio : ${producto.precio}</p>
-//     <p>Cantidad: <span id="cantidad">${producto.cantidad}</span></p>
-//     <button onclick="eliminarDelCarrito(${producto.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`
-
-//         contenedorCarrito.appendChild(div)
-
-//         localStorage.setItem('carrito', JSON.stringify(carrito))
-//     })
-
-//     contadorCarrito.innerText = carrito.length
-//     console.log(carrito)
-//     precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
-//     //rest parameters
-// }
+const pagar = document.getElementById("pagar")
